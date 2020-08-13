a519a1b9a722d9e3818657e7409308828c850213
jlassi1 <khawlajlassi1990@gmail.com>
2020-08-13 17:01:48 +0100

    task10

--- models/place.py
+++ models/place.py
@@ -5,6 +5,24 @@ from sqlalchemy import Column, String, ForeignKey, Integer, Float, Table
 from models.review import Review
 from sqlalchemy.orm import relationship
 from models.amenity import Amenity
+place_amenity = Table
+    "place_amenity",
+    Base.metadata,
+    Column(
+        "place_id",
+        String(60),
+        ForeignKey("places.id"),
+        primary_key=True,
+        nullable=False
+    ),
+    Column(
+        "amenity_id",
+        String(60),
+        ForeignKey("amenities.id"),
+        primary_key=True,
+        nullable=False
+    )
+)
 
 
 class Place(BaseModel, Base):
@@ -70,24 +88,6 @@ class Place(BaseModel, Base):
         viewonly=False
     )
 
-    place_amenity = Table(
-        "place_amenity",
-        Base.metadata,
-        Column(
-            "place_id",
-            String(60),
-            ForeignKey("places.id"),
-            primary_key=True,
-            nullable=False
-        ),
-        Column(
-            "amenity_id",
-            String(60),
-            ForeignKey("amenities.id"),
-            primary_key=True,
-            nullable=False
-        )
-    )
 
     @property
     def reviews(self):
