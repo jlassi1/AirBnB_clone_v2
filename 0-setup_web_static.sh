cb4be80d4bfc19c138b5b3647aa96bacb212b1fa
jlassi1 <khawlajlassi1990@gmail.com>
2020-08-17 12:33:01 +0100

    0-setup_web_static.sh

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+# sets up your web servers for the deployment of web_static.
+
+sudo apt-get -y install nginx
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+sudo ln -sf  /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo printf %s"
+server {
+    listen 80;
+    listen [::]:80 default_server;
+    server_name https://khawla.tech/
+    root   /var/www//html;
+    index index.nginx-debian.html index.nginx-debian.html;
+    add_header X-Served-By 1391-web-01-1596560744;
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+    location /current/ {
+        alias  /data/web_static/current/hbnb_static;
+    }
+}" > /etc/nginx/sites-available/default
+sudo service nginx restart
